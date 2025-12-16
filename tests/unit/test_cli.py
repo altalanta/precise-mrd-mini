9a5981aea1f8c4c2ac9fd5eddfc0889f8ce4bbfb
Altalanta <131017874+altalanta@users.noreply.github.com>
2025-12-16 10:32:59 -0800

    Add files via upload

--- tests/unit/test_cli.py
+++ tests/unit/test_cli.py
@@ -10,3 +10,4 @@ def test_cli_help():
     assert result.exit_code == 0
     assert "Usage: main [OPTIONS] COMMAND [ARGS]..." in result.output
     assert "Deterministic ctDNA/UMI MRD pipeline" in result.output
+
